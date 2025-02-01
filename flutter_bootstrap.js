bebb9e34192c5b0801b73836e71819cf2571845c
brunodavi <brunodaviandrade2000@gmail.com>
2025-02-01 20:00:57 +0000

    Deploying to gh-pages from @ brunodavi/direct_link_generator@2f67481bd3f385d665c8f83a7c4ec5a1d2456099 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4125307635"
+    serviceWorkerVersion: "3999396781"
   }
 });
